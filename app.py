a8306aeefa24a97e9b071634060872bb67c9e6b7
Grant T. Aguinaldo <grant.aguinaldo@gmail.com>
2018-03-19 23:33:37 -0700

    Added database and app files for API -gta.

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, jsonify
+from database import character, location
+
+app = Flask(__name__)
+
+
+@app.route('/characters')
+def characters():
+
+    character_output = character()
+    return (jsonify(character_output))
+
+
+@app.route('/locations')
+def locations():
+
+    location_output = location()
+    return (jsonify(location_output))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,28 @@
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, inspect
+
+engine = create_engine("sqlite:///data/simpsons_db.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+inspector = inspect(engine)
+
+Simpsons_characters = Base.classes.simpsons_characters
+Simpsons_episodes = Base.classes.simpsons_episodes
+Simpsons_locations = Base.classes.simpsons_locations
+Simpsons_script_lines = Base.classes.simpsons_script_lines
+
+session = Session(engine)
+
+
+def character():
+    queryExpression = session.query(Simpsons_characters)
+    characterList = [each.normalized_name for each in queryExpression]
+    return characterList
+
+
+def location():
+    queryExpression = session.query(Simpsons_locations)
+    locationList = [each.normalized_name for each in queryExpression]
+    return locationList
