19bf705a62d8adfd2bc267add94596586833f410
GTA <grant.aguinaldo@gmail.com>
2018-03-25 00:11:31 -0700

    js work -gta

--- static/js/app.js
+++ static/js/app.js
@@ -12,3 +12,30 @@ d3.select('.blockquote')
     .attr('class', 'blockquote-footer')
     .text(response[0]['character'])
 });
+
+
+var namesUrl = "top_50_character";
+Plotly.d3.json(namesUrl, function(error, response) {
+    if (error) return console.warn(error);
+
+    for (var i = 0; i < response.length; i++){
+        d3.select("#simpsonsDropdown")
+              .append('option')
+              .attr('value', response[i])
+              .text(response[i])
+    }
+
+});
+
+// function getSimpsonsData() {
+//         sampleValue = document.getElementById("simpsonsDropdown").value;
+
+//         var endPointCharacterData = '/character_metadata/' + sampleValue
+//         Plotly.d3.json(endPointCharacterData, function(error, response) {
+
+//             if (error) return console.warn(error);
+
+//             console.log(response[0]['Occupation'])
+
+//         });
+// };
