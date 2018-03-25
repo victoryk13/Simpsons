4eccd20b0bc587a871e7cf86ed371a4948736ee5
GTA <grant.aguinaldo@gmail.com>
2018-03-25 16:30:13 -0700

    Added in metadata to page -gta

--- static/js/app.js
+++ static/js/app.js
@@ -14,7 +14,7 @@ d3.select('.blockquote')
 });
 
 
-var namesUrl = "top_50_character";
+var namesUrl = "top_50_characters";
 Plotly.d3.json(namesUrl, function(error, response) {
     if (error) return console.warn(error);
 
@@ -27,15 +27,112 @@ Plotly.d3.json(namesUrl, function(error, response) {
 
 });
 
-// function getSimpsonsData() {
-//         sampleValue = document.getElementById("simpsonsDropdown").value;
+function appendInnerHMTL(response) {
 
-//         var endPointCharacterData = '/character_metadata/' + sampleValue
-//         Plotly.d3.json(endPointCharacterData, function(error, response) {
+              d3.select("#metaList")
+              .append('li')
+              .attr('class', 'list-group-item d-flex justify-content-between align-items-center')
+              .text('Character Name')
+              .append('span')
+              .attr('class', 'badge badge-primary badge-pill')
+              .text(response[0]['Character Name'])
 
-//             if (error) return console.warn(error);
+            d3.select("#metaList")
+              .append('li')
+              .attr('class', 'list-group-item d-flex justify-content-between align-items-center')
+              .text('First Appearance')
+              .append('span')
+              .attr('class', 'badge badge-primary badge-pill')
+              .text(response[0]['First Appearance'])
 
-//             console.log(response[0]['Occupation'])
+            d3.select("#metaList")
+              .append('li')
+              .attr('class', 'list-group-item d-flex justify-content-between align-items-center')
+              .text('Flesch Readability')
+              .append('span')
+              .attr('class', 'badge badge-primary badge-pill')
+              .text(response[0]['Flesch Readability'])
 
-//         });
-// };
+            d3.select("#metaList")
+              .append('li')
+              .attr('class', 'list-group-item d-flex justify-content-between align-items-center')
+              .text('Flesch-Kincaid Grade')
+              .append('span')
+              .attr('class', 'badge badge-primary badge-pill')
+              .text(response[0]['Flesch-Kincaid Grade'])
+
+            d3.select("#metaList")
+              .append('li')
+              .attr('class', 'list-group-item d-flex justify-content-between align-items-center')
+              .text('Gender')
+              .append('span')
+              .attr('class', 'badge badge-primary badge-pill')
+              .text(response[0]['Gender'])
+
+            d3.select("#metaList")
+              .append('li')
+              .attr('class', 'list-group-item d-flex justify-content-between align-items-center')
+              .text('Hair')
+              .append('span')
+              .attr('class', 'badge badge-primary badge-pill')
+              .text(response[0]['Hair'])
+
+            d3.select("#metaList")
+              .append('li')
+              .attr('class', 'list-group-item d-flex justify-content-between align-items-center')
+              .text('Occupation')
+              .append('span')
+              .attr('class', 'badge badge-primary badge-pill')
+              .text(response[0]['Occupation'])
+
+            d3.select("#metaList")
+              .append('li')
+              .attr('class', 'list-group-item d-flex justify-content-between align-items-center')
+              .text('Sentence Count')
+              .append('span')
+              .attr('class', 'badge badge-primary badge-pill')
+              .text(response[0]['Sentence Count'])
+
+            d3.select("#metaList")
+              .append('li')
+              .attr('class', 'list-group-item d-flex justify-content-between align-items-center')
+              .text('Syllable Count')
+              .append('span')
+              .attr('class', 'badge badge-primary badge-pill')
+              .text(response[0]['Syllable Count'])
+
+            d3.select("#metaList")
+              .append('li')
+              .attr('class', 'list-group-item d-flex justify-content-between align-items-center')
+              .text('Word Count')
+              .append('span')
+              .attr('class', 'badge badge-primary badge-pill')
+              .text(response[0]['Word Count'])
+
+            d3.select("#metaList")
+              .append('li')
+              .attr('class', 'list-group-item d-flex justify-content-between align-items-center')
+              .text('Status')
+              .append('span')
+              .attr('class', 'badge badge-primary badge-pill')
+              .text(response[0]['Status'])
+
+
+}
+
+function getSimpsonsData() {
+
+        sampleValue = document.getElementById("simpsonsDropdown").value;
+
+        document.getElementById("metaList").innerHTML = ""
+
+        var endPointCharacterData = '/character_metadata/' + sampleValue
+        Plotly.d3.json(endPointCharacterData, function(error, response) {
+
+            if (error) return console.warn(error);
+
+            appendInnerHMTL(response)
+
+        });
+};
+
