04d7d4a2a35b207bf8bfac3624bb3867153852a4
GTA <grant.aguinaldo@gmail.com>
2018-03-26 19:23:14 -0700

    added google fonts.

--- static/js/homer.js
+++ static/js/homer.js
@@ -1,10 +1,10 @@
 function homerMeta(obj){
-clickX = window.event.x-obj.offsetLeft;
-clickY = window.event.y-obj.offsetTop;
-    if((clickX>=100&&clickX<=200)&&(clickY>=100&&clickY<=200))
-    {
-    alert(" You are between (100,100) And (200,200) of the Image");
-    }
-}
 
-console.log('Hi')
+    console.log('Hi1')
+
+    x=obj.clientX;
+    y=obj.clientY;
+    alert("Your Mouse Position Is : " + x + " and " + y);
+    }
+
+console.log('Hi3')
