d2b2b4a23cd03b83d74bc54cb510a9c1e62d09f5
sungwonb <sungwonbyun93@gmail.com>
2018-03-31 00:51:10 -0700

    final map edit -sb

--- static/js/map.js
+++ static/js/map.js
@@ -1,5 +1,5 @@
 var myMap = L.map("locationMap", {
-    center: [40.42, -3.70],
+    center: [12, -15],
     zoom: 2
   });
 
@@ -9,13 +9,13 @@ L.tileLayer(
     "T6YbdDixkOBWH_k9GbS8JQ"
 ).addTo(myMap);
 
-
+console.log(mapData)
 for (var i =0; i < mapData.length; i++) {
     var place = mapData[i];
-    var popupText = "<h1>Episodes</h1><hr>";
+    var popupText = "<h3>"+place.location+"</h3><hr>";
 
     for (var j=0; j<place.visits.length;j++){
-        popupText = popupText+"<h2>"+place.visits[j].title+"</h2><br><h3>Season: "+place.visits[j].season+"<br>Episode: "+place.visits[j].episode+"</h3><br><br>";
+        popupText = popupText+"<h4 id='mapHeaders'>"+place.visits[j].title+"</h4><h4 id='mapText'>Season: "+place.visits[j].season+"<br>Episode: "+place.visits[j].episode+"</h4><br>";
     }
 
     var marker = L.marker(place.coordinates,{
